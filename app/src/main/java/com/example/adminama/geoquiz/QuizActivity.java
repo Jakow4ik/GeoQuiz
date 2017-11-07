b0f1f3a24a632db4f9ca927747a150cc880b4b3b
Nikolay Yakovchik <jakow4ik@gmail.com>
2017-11-07 18:51:09 +0300

    Закоментирована строка для дальнейшего поиска ошибки в приложении.

--- app/src/main/java/com/example/adminama/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/adminama/geoquiz/QuizActivity.java
@@ -44,7 +44,7 @@ public class QuizActivity extends AppCompatActivity {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
         }
 
-        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        //mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
