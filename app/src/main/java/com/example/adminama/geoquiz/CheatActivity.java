fbf08254e3277dbfce42a2cee070c43b79bedd5b
Nikolay Yakovchik <jakow4ik@gmail.com>
2017-11-12 00:08:37 +0300

    Поворот CheatActivity больше не приводит к сбросу параметров.

--- app/src/main/java/com/example/adminama/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/adminama/geoquiz/CheatActivity.java
@@ -1,9 +1,11 @@
 package com.example.adminama.geoquiz;
 
+import android.animation.Animator;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,8 +15,11 @@ public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.example.adminama.geoquiz.answer_is_true";
     private static final String EXTRA_ANSWER_SHOW = "com.example.adminama.geoquiz.answer_shown";
+    private static final String KEY_CHEAT = "cheat";
+    private static final String KEY_VIEW = "view";
 
     private boolean mAnswerIsTrue;
+    private boolean chekCheat;
 
     private TextView mAnswerTextView;
     private Button mShowAnswerButton;
@@ -38,7 +43,7 @@ public class CheatActivity extends AppCompatActivity {
 
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
         mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
-        mShowAnswerButton.setOnClickListener(new View.OnClickListener(){
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mAnswerIsTrue) {
@@ -48,14 +53,36 @@ public class CheatActivity extends AppCompatActivity {
                 }
 
                 setAnswerShownReult(true);
+                chekCheat = true;
+
+
             }
         });
-        }
+    }
 
-        private void setAnswerShownReult(boolean isAnswerShown) {
-            Intent data = new Intent();
-            data.putExtra(EXTRA_ANSWER_SHOW, isAnswerShown);
-            setResult(RESULT_OK, data);
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString(KEY_VIEW, mAnswerTextView.getText().toString());
+        savedInstanceState.putBoolean(KEY_CHEAT, chekCheat);
+
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle restoreInstanceState) {
+        super.onRestoreInstanceState(restoreInstanceState);
+        mAnswerTextView.setText(restoreInstanceState.getString(KEY_VIEW));
+
+        if (restoreInstanceState.getBoolean(KEY_CHEAT)) {
+            setAnswerShownReult(true);
+            chekCheat = true;
         }
     }
+
+    private void setAnswerShownReult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOW, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+}
 
